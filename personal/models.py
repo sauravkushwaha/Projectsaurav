7ae6c70783d3da45add64c55179d227b15440358
saurav <saurav9695@gmail.com>
2021-04-18 21:53:24 +0530

    initial commit

---
+++ personal/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+#PRIORITY = [
+##	("m", "medium"),
+#	("l", "low"),
+#]
+#
+##	title					=models.CharField(max_length=60)
+#	Question				=models.TextField(max_length=400)
+#	priority				=models.CharField(max_length=1, choices=PRIORITY)
+##	def __str__(self):
+#		return self.title
+##	class Meta:
+##		verbose_name_plural = "Peoples Questions"
+
+
